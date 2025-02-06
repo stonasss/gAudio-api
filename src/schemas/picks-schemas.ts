3d793c045821da40c4b1498dbadf6157b07c8d81
stonasss <luis.b.santos96@gmail.com>
2025-02-05 23:54:59 -0300

    patch: proper pick validation

--- src/schemas/picks-schemas.ts
+++ src/schemas/picks-schemas.ts
@@ -2,7 +2,8 @@ import Joi from "joi";
 
 export const pickSchema = Joi.object({
     image: Joi.string().uri().required(),
-    title: Joi.string().allow(""),
+    title: Joi.string().required(),
+    artist: Joi.string().required(),
     description: Joi.string().required(),
     link: Joi.string().allow(""),
 });
